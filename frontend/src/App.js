c0ec70969da5827f1206906ba70af84706f030a2
Artur Maizelev <arturm0808@gmail.com>
2020-10-18 09:57:29 +0300

    Fetch All Messages ☘️

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Pusher from "pusher-js";
+import axios from "axios";
 
 import MainRoom from "./components/MainRoom/MainRoom";
 import Sidebar from "./components/Sidebar/Sidebar";
@@ -7,6 +8,17 @@ import Sidebar from "./components/Sidebar/Sidebar";
 import "./style.scss";
 
 const App = () => {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    async function fetchMyAPI() {
+      const messages = await axios.get(`/messages`);
+      setMessages(messages.data.messages);
+    }
+
+    fetchMyAPI();
+  }, []);
+
   useEffect(() => {
     const pusher = new Pusher("ed615816d21fdd790ef8", {
       cluster: "eu",
